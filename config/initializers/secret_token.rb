ea70a2e32627e8dd4ce088aea3b83eaee25b85d2
Michael Hoffert <mike.hoffert@gmail.com>
2013-01-29 10:09:09 -0600

    Commit core rails environment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Familysearch::Application.config.secret_token = 'bc69a54ba8bafb43e3d50713d40af10ececc7fa8b6155ccd5d56bb49211f8857204a7e3727af4e9d12f4ce7d12bf29b4f2527167c87e3b835634a62c7b747c28'

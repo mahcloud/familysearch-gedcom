ef6f04e36ceefc37c3b105b53107140777baed36
Michael Hoffert <mike.hoffert@gmail.com>
2013-02-05 13:53:41 -0600

    Validation on user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password
+  
+  validates :email, :uniqueness => true,
+  	:length => { :within => 5..50 },
+	:format => { :with => /^[^@[\w.-]+@[\w.-]+[.][a-z]{2,4}$/i }
+
   has_one :family_search_user
 end
